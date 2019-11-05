04d01a555fd13cbcf3680507a99cac1bd154603c
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-11-05 12:26:48 -0500

    added uniform form styling to change password components

--- src/components/auth/ChangePassword.js
+++ src/components/auth/ChangePassword.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import FormErrors from "../FormErrors";
 import Validate from "../utility/FormValidation";
 import { Auth } from 'aws-amplify';
+import './forms.css'
 
 class ChangePassword extends Component {
   state = {
@@ -67,8 +68,8 @@ class ChangePassword extends Component {
   render() {
     return (
       <section className="section auth">
-        <div className="container">
-          <h1>Change Password</h1>
+        <div className="form-container">
+          <h1 className="form-header">Change Password</h1>
           <FormErrors formerrors={this.state.errors} />
 
           <form onSubmit={this.handleSubmit}>
@@ -117,14 +118,14 @@ class ChangePassword extends Component {
                 </span>
               </p>
             </div>
-            <div className="field">
+            <div className="form-bottom">
               <p className="control">
-                <a href="/forgotpassword">Forgot password?</a>
+                <a className="forgot-link" href="/forgotpassword">Forgot password?</a>
               </p>
             </div>
             <div className="field">
               <p className="control">
-                <button className="button is-success">
+                <button className="submit-btn is-success">
                   Change password
                 </button>
               </p>
--- src/components/auth/ChangePasswordConfirm.js
+++ src/components/auth/ChangePasswordConfirm.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import './forms.css'
 
 class ChangePasswordConfirmation extends Component {
   render() {
     return (
       <section className="section auth">
-        <div className="container">
-          <h1>Change Password</h1>
-          <p>Your password has been successfully updated!</p>
+        <div className="form-container">
+          <h1 className="form-header">Change Password</h1>
+          <p className="directions">Your password has been successfully updated!</p>
         </div>
       </section>
     );
