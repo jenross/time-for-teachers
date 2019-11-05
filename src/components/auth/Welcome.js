d83780b500ddf216b4f9153f4cf4e3443bfc03e5
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-11-05 12:41:03 -0500

    added initial styles for welcome component

--- src/components/auth/Welcome.js
+++ src/components/auth/Welcome.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './forms.css'
 
 export default function Welcome() {
   return (
     <section className="section auth">
-      <div className="container">
-        <h1>Welcome!</h1>
-        <p>You have successfully registered a new account.</p>
-        <p>We've sent you a email. Please enter the code to verify your account.</p>
+      <div className="form-container">
+        <h1 className="form-header">Welcome!</h1>
+        <p className="directions">You have successfully registered a new account.</p>
+        <p className="directions">We've sent you a email. Please enter the code to verify your account.</p>
       </div>
     </section>
   )
