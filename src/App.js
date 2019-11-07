aecb7b8b884459c6148e0be9df79b43735ed6c19
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-11-07 18:56:42 -0500

    routing and auth fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import AuthNavbar from './components/AuthNavbar';
-import Home from './components/Home';
-import LogIn from './components/auth/LogIn';
-import Register from './components/auth/Register';
-import ForgotPassword from './components/auth/ForgotPassword';
-import ForgotPasswordVerification from './components/auth/ForgotPasswordVerification';
-import ChangePassword from './components/auth/ChangePassword';
-import ChangePasswordConfirm from './components/auth/ChangePasswordConfirm';
-import Welcome from './components/auth/Welcome';
-import Footer from './components/Footer';
-import Dashboard from './components/Dashboard';
-import { Auth } from 'aws-amplify';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import "./App.css";
+import AuthNavbar from "./components/AuthNavbar";
+import Home from "./components/Home";
+import LogIn from "./components/auth/LogIn";
+import Register from "./components/auth/Register";
+import ForgotPassword from "./components/auth/ForgotPassword";
+import ForgotPasswordVerification from "./components/auth/ForgotPasswordVerification";
+import ChangePassword from "./components/auth/ChangePassword";
+import ChangePasswordConfirm from "./components/auth/ChangePasswordConfirm";
+import Welcome from "./components/auth/Welcome";
+import Footer from "./components/Footer";
+import Dashboard from "./components/Dashboard";
+import { Auth } from "aws-amplify";
 
 // styles
 import "assets/css/bootstrap.min.css";
@@ -22,65 +27,106 @@ import "assets/demo/react-demo.css";
 import "assets/demo/nucleo-icons-page-styles.css";
 
 class App extends Component {
-
   state = {
     isAuthenticated: false,
     isAuthenticating: true,
     user: null
-  }
+  };
 
   setAuthStatus = authenticated => {
     this.setState({ isAuthenticated: authenticated });
-  }
+  };
 
   setUser = user => {
     this.setState({ user: user });
-  }
+  };
 
   async componentDidMount() {
     try {
       // const session = await Auth.currentSession();
-      //true or false here? 
-      this.setAuthStatus(true);
+      //true or false here?
       const user = await Auth.currentAuthenticatedUser();
+      this.setAuthStatus(true);
       this.setUser(user);
-    } catch(error) {
-      if (error !== 'No current user') {
+    } catch (error) {
+      if (error !== "No current user") {
         console.log(error);
       }
     }
-  
+
     this.setState({ isAuthenticating: false });
   }
- 
+
   render() {
     const authProps = {
       isAuthenticated: this.state.isAuthenticated,
       user: this.state.user,
       setAuthStatus: this.setAuthStatus,
       setUser: this.setUser
-    }
+    };
     return (
-      !this.state.isAuthenticating &&
-      <div className="App">
-        <Router>
-          <div>
+      !this.state.isAuthenticating && (
+        <div className="App">
+          <Router>
+            <Redirect to={this.state.authenticated ? "/dashboard" : "/"} />
             <AuthNavbar auth={authProps} />
             <Switch>
-              <Route exact path="/" render={(props) => <Home {...props} auth={authProps} />} />
-              <Route exact path="/login" render={(props) => <LogIn {...props} auth={authProps} />} />
-              <Route exact path="/register" render={(props) => <Register {...props} auth={authProps} />} />
-              <Route exact path="/forgotpassword" render={(props) => <ForgotPassword {...props} auth={authProps} />} />
-              <Route exact path="/forgotpasswordverification" render={(props) => <ForgotPasswordVerification {...props} auth={authProps} />} />
-              <Route exact path="/changepassword" render={(props) => <ChangePassword {...props} auth={authProps} />} />
-              <Route exact path="/changepasswordconfirmation" render={(props) => <ChangePasswordConfirm {...props} auth={authProps} />} />
-              <Route exact path="/welcome" render={(props) => <Welcome {...props} auth={authProps} />} />
-              <Route exact path="/dashboard" render={(props) => <Dashboard {...props} auth={authProps} />} />
+              <Route
+                exact
+                path="/"
+                render={props => <Home {...props} auth={authProps} />}
+              />
+              <Route
+                exact
+                path="/login"
+                render={props => <LogIn {...props} auth={authProps} />}
+              />
+              <Route
+                exact
+                path="/register"
+                render={props => <Register {...props} auth={authProps} />}
+              />
+              <Route
+                exact
+                path="/forgotpassword"
+                render={props => <ForgotPassword {...props} auth={authProps} />}
+              />
+              <Route
+                exact
+                path="/forgotpasswordverification"
+                render={props => (
+                  <ForgotPasswordVerification {...props} auth={authProps} />
+                )}
+              />
+              <Route
+                exact
+                path="/changepassword"
+                render={props => <ChangePassword {...props} auth={authProps} />}
+              />
+              <Route
+                exact
+                path="/changepasswordconfirmation"
+                render={props => (
+                  <ChangePasswordConfirm {...props} auth={authProps} />
+                )}
+              />
+              <Route
+                exact
+                path="/welcome"
+                render={props => <Welcome {...props} auth={authProps} />}
+              />
+              {this.state.isAuthenticated && (
+                <Route
+                  exact
+                  path="/dashboard"
+                  render={props => <Dashboard {...props} auth={authProps} />}
+                />
+              )}
             </Switch>
             <Footer />
-          </div>
-        </Router>
-      </div>
+          </Router>
+        </div>
+      )
     );
   }
 }
--- src/components/AuthNavbar.js
+++ src/components/AuthNavbar.js
@@ -1,90 +1,71 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
-import { Auth } from 'aws-amplify';
-import './AuthNavbar.css';
-import {
-  Button,
-  NavbarBrand,
-  Navbar,
-  NavItem,
-  Nav
-} from "reactstrap";
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Auth } from "aws-amplify";
+import "./AuthNavbar.css";
+import { Button, NavbarBrand, Navbar, NavItem, Nav } from "reactstrap";
 
-
-
-export default class AuthNavbar extends Component  {
+class AuthNavbar extends Component {
   handleLogOut = async event => {
     event.preventDefault();
     try {
       Auth.signOut();
       this.props.auth.setAuthStatus(false);
       this.props.auth.setUser(null);
-    }catch(error) {
+      this.props.history.push("/");
+    } catch (error) {
       console.log(error.message);
     }
-  }
+  };
   render() {
+    console.log("the props", this.props);
     return (
       <Navbar className="navbar-absolute navbar-transparent" expand="lg">
         <NavbarBrand to="/" tag={Link} id="navbar-brand">
           Time for Teachers
         </NavbarBrand>
         <Nav className="ml-auto" id="ceva" navbar>
-              <NavItem>
-                {!this.props.auth.isAuthenticated && (
-                    <Button
-                      className="nav-link"
-                      id="register-btn"
-                      color="primary"
-                      href="/register"
-                    >
-                      
-                      <p>Register</p>
-                      
-                    </Button>
-                  )}
-                </NavItem>
-                
-                
-                <NavItem>
-                  {!this.props.auth.isAuthenticated && (
-                    <Button
-                      className="nav-link"
-                      id="login-btn"
-                      color="neutral"
-                      href="/login"
-                    >
-                      
-                      <p>Sign In</p>
-                      
-                    </Button>
-                  )}
-                </NavItem>
-                
-          
-       
-            
-              <NavItem>
-                {this.props.auth.isAuthenticated && (
-                    <Button
-                      className="nav-link"
-                      id="logout-btn"
-                      color="neutral"
-                      href="/"
-                      onClick={this.handleLogOut}
-                    >
-                      <p>Log Out</p>
-                      
-                    </Button>
-                )}
-              </NavItem>
-          
-       
-        </Nav>
+          <NavItem>
+            {!this.props.auth.isAuthenticated && (
+              <Button
+                className="nav-link"
+                id="register-btn"
+                color="primary"
+                href="/register"
+              >
+                <p>Register</p>
+              </Button>
+            )}
+          </NavItem>
 
+          <NavItem>
+            {!this.props.auth.isAuthenticated && (
+              <Button
+                className="nav-link"
+                id="login-btn"
+                color="neutral"
+                href="/login"
+              >
+                <p>Sign In</p>
+              </Button>
+            )}
+          </NavItem>
+
+          <NavItem>
+            {this.props.auth.isAuthenticated && (
+              <Button
+                className="nav-link"
+                id="logout-btn"
+                color="neutral"
+                href="/"
+                onClick={this.handleLogOut}
+              >
+                <p>Log Out</p>
+              </Button>
+            )}
+          </NavItem>
+        </Nav>
       </Navbar>
-      
     );
   }
 }
-// export default AuthNavbar;
+export default withRouter(AuthNavbar);
