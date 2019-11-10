39082c594633650de534bdf512282e5ac00c1150
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-11-10 16:28:58 -0500

    adjusted positioning of navbar; updated hero img and text; starting description; added footer content

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,77 @@
 import React from 'react'
-import './Footer.css'
+// import './Footer.css'
 
-export default function Footer() {
-    return (
-      <footer>
-        <div className='footer-content'>
-          <p>
-            © 2019
-          </p>
-        </div>
+// export default function Footer() {
+//     return (
+//       <footer>
+//         <div className='footer-content'>
+//           <p>
+//             © 2019
+//           </p>
+//         </div>
+//       </footer>
+//     )
+// }
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+// core components
+
+function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <Container>
+          <nav>
+            <ul>
+              <li>
+                <a
+                  href="https://www.creative-tim.com?ref=nuk-pro-react-footer"
+                  target="_blank"
+                >
+                  Creative Tim
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://presentation.creative-tim.com?ref=nuk-pro-react-footer"
+                  target="_blank"
+                >
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://blog.creative-tim.com?ref=nuk-pro-react-footer"
+                  target="_blank"
+                >
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="copyright" id="copyright">
+            © {new Date().getFullYear()}, Designed by{" "}
+            <a
+              href="https://www.invisionapp.com?ref=creativetim"
+              target="_blank"
+            >
+              Invision
+            </a>
+            . Coded by{" "}
+            <a
+              href="https://www.creative-tim.com?ref=nuk-pro-react-footer"
+              target="_blank"
+            >
+              Creative Tim
+            </a>
+            .
+          </div>
+        </Container>
       </footer>
-    )
+    </>
+  );
 }
+
+export default Footer;
